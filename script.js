556016abfb1dc8c3250af73a9d6d6622155a31ae
Rhys Smoker <r.smoker@gmail.com>
2022-11-04 12:00:24 -0700

    made a getweather function and a putweatherinDOM function

--- script.js
+++ script.js
@@ -3,52 +3,80 @@ var searcherEl = document.querySelector("#searcher");
 
 //DOM hooks for elements that will display feedback
 var forcastEl = document.querySelector("#forcast");
+var rhysApiKey = "8efdcf6890084b049f69cd42d7792cd8";
 
 function handleSearcherSubmit(event) {
-	//takes the submit event and does stuff with it
-	var cityInput = document.querySelector("#searcher-input").value; ////gets the text (city) the user types in
-	var rhysApiKey = "8efdcf6890084b049f69cd42d7792cd8";
 	event.preventDefault();
+	//console.log("button clicks");
+	//takes the submit event and does stuff with it
+	var cityInput = document.querySelector("#searcher-input").value; ////gets the text (the city name) the user types in
+
 	function getLatLon() {
 		var latLonQueryUrl =
 			"http://api.openweathermap.org/geo/1.0/direct?q=" +
 			cityInput +
-			",&appid=" +
+			"&appid=" +
 			rhysApiKey;
-		console.log(latLonQueryUrl);
 
 		fetch(latLonQueryUrl)
-        .then
-        (function (response) 
-            {
-                if (!response.ok) {
-                    throw response.json();
-                }
-            return response.json(); 
-            }
-        );
-        }
-	    .then(function (coordinates) { //hopefully sets the response to a var called "coordinates"
-            console.log("the coordinates are "+coordinates);
-	    })
-	getLatLon(); //whats wrong with calling gtLatLon at this point?
+			.then(function (response) {
+				//waiting for fetch promise to resolve
+				if (!response.ok) {
+					throw response.json();
+				}
+				return response.json(); //rehydrates
+			})
+			.then(function (coordinates) {
+				//coordinates is now an object
+				//hopefully sets the response to a var called "coordinates"
+				console.log(coordinates);
+				var lat = coordinates[0].lat;
+				var lon = coordinates[0].lon;
+				console.log(lat, lon);
+				getWeather(lat, lon); //passes lat and lon to the getWeather function
+			});
+	}
+	getLatLon();
 }
-searcherEl.addEventListener("submit", handleSearcherSubmit); // executes the above event handler function when a form submit happens
 
-//maybe  do a .then(function
-//then update the DOM
+function getWeather(lat, lon) {
+	//receives lat and lon as first and second thing passed
+	var weatherQueryURL =
+		"http://api.openweathermap.org/data/2.5/forecast?lat=" +
+		lat +
+		"&lon=" +
+		lon +
+		"&appid=" +
+		rhysApiKey;
+
+	fetch(weatherQueryURL)
+		.then(function (response) {
+			//waiting for fetch promise to resolve
+			if (!response.ok) {
+				throw response.json();
+			}
+			return response.json(); //rehydrates
+		})
+		.then(function (weather) {
+			console.log(weather);
+			putWeatherinDOM(weather); //passes the weather object to putWeatherinDOM
+		});
+}
+
+function putWeatherinDOM(weatherstuff) {
+	//receives the weather object and renames it just for fun
+	var firstDayDate = weatherstuff.list[0].dt_txt; //use brackets to drill down on array, dot notation to drill down on objects
+	forcastEl.append(firstDayDate);
+}
+
+searcherEl.addEventListener("submit", handleSearcherSubmit); // executes handleSearcherSubmit when a form submit happens to searcherEl
 
 //Put in a function to update the DOM with query results
 
 /////////////
 
 //API call for lat and long exampl
-//Example of API call
-
 // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
+
 //API call for weather format
 //api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
-
-//var lat =
-//var lon =
-//var Key =      //API ke
